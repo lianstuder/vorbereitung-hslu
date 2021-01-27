163065185a5541ce978e5d718f41d05eccda5030
Lian Studer <ln.studer@protonmail.ch>
2021-01-27 11:59:45 +0100

    example for vue component

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>My Projects</h1>
+    <h2>Lian Sutder</h2>
+    <p><a href="https://github.com/lianstuder">GitHub</a></p>
+    <ul>
+      <li><Project name="lit project" description="haha lmao"></Project></li>
+      <li><Project name="litter project" description="haha lmao liiit"></Project></li>
+    </ul>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Project from "./components/Project.vue";
+
+@Component({
+  components: {
+    Project
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Project.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="project">
+    <h2>{{ name }}</h2>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Project extends Vue {
+  @Prop() private name!: string;
+  @Prop() private description!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
