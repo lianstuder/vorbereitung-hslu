21134ae496a8704853a3b932b90e3a6e4cf0b9b6
Lian Studer <ln.studer@protonmail.ch>
2021-01-28 11:30:41 +0100

    add controller to API

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,26 +1,31 @@
 import App from "./app";
 
 import * as bodyParser from "body-parser";
-import * as mongoose from "mongoose"
+import mongoose from "mongoose"
 
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import ProjectController from "./controllers/project.controller";
+
 const uri: string = process.env.MONGO_URI!.toString();
 
-mongoose.connect(uri, (err: any) => {
+mongoose.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}, (err: any) => {
     if (err) return console.error(err.message);
     console.log("Connected to database.");
-})
+});
 
 new App({
     port: 5000,
     controllers: [
-
+        new ProjectController()
     ],
     middlewares: [
         bodyParser.json(),
         bodyParser.urlencoded({ extended: true })
     ]
-})
+}).listen();
 
