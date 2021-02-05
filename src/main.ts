8c11bee651c4f98853824b93707853828c1c75a8
Lian Studer <ln.studer@protonmail.ch>
2021-02-05 13:12:32 +0100

    edit main.ts: integrate vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Vuetify from "vuetify";
 
+import "vuetify/dist/vuetify.min.css";
 import "./assets/scss/main.scss";
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
 
 new Vue({
   render: h => h(App)
